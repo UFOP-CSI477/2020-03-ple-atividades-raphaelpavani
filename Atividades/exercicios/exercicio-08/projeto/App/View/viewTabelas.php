8daba7090e4c74871e47559cfadfdea386e01456
raphaelpavani <raphaelpavani@gmail.com>
2020-09-21 23:50:08 -0300

    materialize css+view;

---
+++ Atividades/exercicios/exercicio-08/projeto/App/View/viewTabelas.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <title>Estados e Produtos</title>
+</head>
+
+<body>
+  <div class="container">
+    <h2 class="blue-text text-darken-2">Estados</h2>
+    <table class="highlight responsive-table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Nome</th>
+          <th>Sigla</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        while ($e = $estados->fetch()) {
+          echo "
+          <tr>
+            <td>" . $e["id"] . "</td>
+            <td>" . $e["nome"] . "</td>
+            <td>" . $e["sigla"] . "</td>
+          </tr>
+          ";
+        } ?>
+      </tbody>
+    </table>
+
+    <h2 class="blue-text text-darken-2">Produtos</h2>
+    <table class="highlight responsive-table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Nome</th>
+          <th>Medida</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        while ($e = $produtos->fetch()) {
+          echo "
+          <tr>
+            <td>" . $e["id"] . "</td>
+            <td>" . $e["nome"] . "</td>
+            <td>" . $e["un"] . "</td>
+          </tr>
+          ";
+        } ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+
+</html>
--- Atividades/exercicios/exercicio-08/projeto/public/index.php
+++ Atividades/exercicios/exercicio-08/projeto/public/index.php
@@ -1,48 +1,15 @@
 <?php
 
-require '../vendor/autoload.php';
+require "../vendor/autoload.php";
 
-use App\Models\Estado;
-use App\Database\Connection;
 use App\Database\AdapterSQLite;
-
-$estado = new Estado(1, "Minas Gerais", "MG");
-var_dump($estado);
-
-$connection = new Connection(new AdapterSQLite());
-var_dump($connection);
-
-$connection->getAdapter()->open();
-
-// ------------------------------------------------------------
-// Implementar na view de Estados ->
-// Imprimir os estados com o adapter implementado:
-$sql = "SELECT * FROM estados";
-$result = $connection->getAdapter()->get()->query($sql);
-
-echo "<hr><ol>";
-
-while ($e = $result->fetch()) {
-    echo "<li>" . $e["nome"] . "-" . $e['sigla'] . "</li>\n";
-}
-$sql = "SELECT * FROM produtos";
-$result = $connection->getAdapter()->get()->query($sql);
-echo "</ol><hr>";
-
-echo "<hr><ol>";
-
-while ($e = $result->fetch()) {
-    echo "<li>" . $e["id"] . "-" . $e["nome"] . "-" . $e['um'] . "</li>\n";
-}
-$sql = "SELECT * FROM cidades";
-$result = $connection->getAdapter()->get()->query($sql);
-while ($e = $result->fetch()) {
-    echo "<li>" . $e["id"] . "-" . $e["nome"] . "-" . $e['estado_id'] . "</li>\n";
-}
-
-echo "</ol><hr>";
+use App\Database\Connection;
 
 
-// ------------------------------------------------------------
+$aux = new Connection(new AdapterSQLite);
+$connection = $aux->getAdapter()->open();
+$estados = $aux->getAdapter()->getEstados($connection);
+$cidades = $aux->getAdapter()->getCidades($connection);
+$produtos = $aux->getAdapter()->getProdutos($connection);
 
-$connection->getAdapter()->close();
+require_once "../App/View/dadosView.php";
