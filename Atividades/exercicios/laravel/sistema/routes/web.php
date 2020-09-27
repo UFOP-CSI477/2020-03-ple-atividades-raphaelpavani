392105f054be237ada20414f071cf05f913ce6d9
raphaelpavani <raphaelpavani@gmail.com>
2020-09-27 19:49:43 -0300

    roteiro das aulas 21 e 22 prontos

--- Atividades/exercicios/laravel/sistema/routes/web.php
+++ Atividades/exercicios/laravel/sistema/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Estado;
+use App\Models\Produto;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/ola', function () {
+    return 'Olá Mundo';
+});
+
+Route::get('/estados/todos  ', function () {
+    $estados = Estado::all();
+    return view('lista', ['dados' => $estados]);
+});
+
+Route::get('/estados/{id}', function ($id) {
+    $estado = Estado::findOrfail($id);
+    if ($estado == null) {
+        return 'ID inválido';
+    }
+    return view('lista', ['dados' => $estado]);
+});
+
+Route::get('/produtos/todos  ', function () {
+    $produtos = Produto::all();
+    return view('lista', ['dados' => $produtos]);
+});
+
+Route::get('/produtos/{id}', function ($id) {
+    $produto = Produto::findOrfail($id);
+    if ($produto == null) {
+        return 'ID inválido';
+    }
+    return view('lista', ['dados' => $produto]);
 });
